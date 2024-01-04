f59a7f0ed3ab03874fcd0052de4028bf5606abae
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2024-01-04 12:15:23 +0530

    Done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
         <div className="contact-main">
           <div className="cardProject">
             <h2>Projects Github & Frontend Mentero</h2>
-            <p>You can visit my project</p>
+            <p>You can see my projects</p>
             <a href="/">Github</a>
             <h3>My Frontend Projects & Clone</h3>
             <a href="/">Frontend</a>
@@ -19,8 +19,17 @@ function Contact() {
           </div>
         </div>
         <div className="footer">
-          <div className="social_link"></div>
-        sacdxas
+          <div className="social_link">
+            <a href="/"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="/"><i class="fa-brands fa-github"></i></a>
+            <a href="/"><i class="fa-brands fa-facebook"></i></a>
+            <a href="/"><i class="fa-brands fa-instagram"></i></a>
+            <a href="/"><i class="fa-brands fa-x-twitter"></i></a>
+        </div>
+        <div className="subFooter">
+        <p>&copy; 2024 Privacy Policy Terms & Conditions </p>
+
+        </div>
         </div>
       </div>
     </>
